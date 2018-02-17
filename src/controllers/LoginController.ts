4ca73f9c83fbd4d4a46c5f1f487c7490ad0309fe
Daniel Isaac Geslin <danielisaacgeslin@gmail.com>
2018-02-17 13:08:21 +0100

    update

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,24 +1,19 @@
 import { Route, Controller, Post, Body } from 'tsoa';
 
-import { ApiError } from '../config/ErrorHandler';
 import { ProvideSingleton, inject } from '../ioc';
-import { UserModel } from '../models';
-import { UserRepository } from '../repositories';
-// import { GetExchangeUseCase } from '../usecases';
+import { LoginUseCase } from '../usecases';
 
 @Route('login')
 @ProvideSingleton(LoginController)
 export class LoginController extends Controller {
 
-  constructor( @inject(UserRepository) private userRepository: UserRepository) {
+  constructor( @inject(LoginUseCase) private loginUseCase: LoginUseCase) {
     super();
   }
 
   @Post()
   public async exchange( @Body() body: { username: string; password: string; }): Promise<{ jwt: string }> {
     const { username, password } = body;
-    const res = await this.userRepository.findOne({ username, password });
-    if (res) return { jwt: 'todo' };
-    else throw new ApiError('UserNotFound', 401, 'user not found');
+    return this.loginUseCase.login(username, password);
   }
 }
